83d297f0db2d8e13ac1deaef6cacc623ffac07b8
dpaguba <dpaguba@gmail.com>
2022-05-08 12:53:29 +0200

    form is created in html

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dialog',
@@ -7,12 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogComponent implements OnInit {
 
-  freshnessList = ["Brand"]
+  brands = ["American Seafood", "Pacific Seafood", "Trident Seafoods", "Bumble Bee Foods", "Other"]
 
-  constructor() { }
+  productForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-    
+    this.productForm = this.formBuilder.group({
+      productName: ['', Validators.required]
+    })
   }
 
 }
