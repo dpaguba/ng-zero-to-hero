4031ec50f5bcc2da1265460c112fbf6c039da6e0
dpaguba <dpaguba@gmail.com>
2022-05-08 19:08:26 +0200

    added pipes, made sort possible

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
-import {Component, Inject, OnInit, ViewChild} from '@angular/core';
+import {Component, Inject, OnInit, ViewChild, AfterViewInit} from '@angular/core';
 import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { DialogComponent } from './dialog/dialog.component';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 import { ApiService } from './services/api.service';
+import { MatSort, Sort } from '@angular/material/sort';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
+
 
 @Component({
   selector: 'app-root',
@@ -13,16 +16,18 @@ import { ApiService } from './services/api.service';
 export class AppComponent implements OnInit {
   title = 'Seafood';
 
-  displayedColumns: string[] = ['date', 'productName', 'category', 'price', 'amount', 'comment'];
-  dataSource !: MatTableDataSource<any>;
+  displayedColumns: string[] = ['date', 'productName', 'category', 'brands', 'price', 'amount', 'comment', 'action'];
+  dataSource !: MatTableDataSource<any>
 
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatPaginator) paginator!: MatPaginator
+  @ViewChild(MatSort) sort!: MatSort
 
   // ngAfterViewInit() {
   //   this.dataSource.paginator = this.paginator;
+  //   this.dataSource.sort = this.sort;
   // }
   
-  constructor(private dialog: MatDialog, private api: ApiService){
+  constructor(private dialog: MatDialog, private api: ApiService, private _liveAnnouncer: LiveAnnouncer){
 
   }
   ngOnInit(): void {
@@ -41,12 +46,30 @@ export class AppComponent implements OnInit {
       next: (res)=>{
         this.dataSource = new MatTableDataSource(res)
         this.dataSource.paginator = this.paginator
-      
+        this.dataSource.sort = this.sort
       },
       error: (err)=>{
         alert("Error while fetching the goods!")
       }
     })
   }
+
+  applyFilter(event: Event){
+    const filterValue = (event.target as HTMLInputElement).value
+    this.dataSource.filter = filterValue.trim().toLowerCase()
+
+    if(this.dataSource.paginator){
+      this.dataSource.paginator.firstPage()
+    }
+  }
+
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
 }
 
