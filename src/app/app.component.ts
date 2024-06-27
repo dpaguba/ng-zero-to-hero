fbd47a06d56494f47d9a9825193a1ec96b8de59c
Dmytro Pahuba <dpaguba@gmail.com>
2024-06-27 19:38:09 +0200

    refactor: Update app.component.ts to inject ProfileService and fetch test accounts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
+import { ProfileService } from './data/services/profile.service';
+import { JsonPipe } from '@angular/common';
+import { Profile } from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProfileCardComponent],
+  imports: [RouterOutlet, ProfileCardComponent, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'tik-talk';
+
+  profileService = inject(ProfileService); 
+  profiles: Profile[] = [];
+
+  constructor() {
+    this.profileService.getTestAccounts().subscribe( val  => {
+      this.profiles = val;
+    });
+  }
 }
