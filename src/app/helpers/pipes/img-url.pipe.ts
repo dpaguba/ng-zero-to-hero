6bec81f3bdb143f7253ccfa81c924181bb8f8564
Dmytro Pahuba <dpaguba@gmail.com>
2024-06-27 19:37:46 +0200

    feat: Add ImgUrlPipe for transforming image URLs

---
+++ src/app/helpers/pipes/img-url.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'imgUrl',
+  standalone: true
+})
+export class ImgUrlPipe implements PipeTransform {
+
+  transform(value: string | null): string | null {
+    if (!value) {
+      // replace with default image
+      return null;
+    }
+    return `https://icherniakov.ru/yt-course/${value}`;
+  }
+
+}
