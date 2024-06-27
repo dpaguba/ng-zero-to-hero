65619099b58a1ef8fe2dcd5d3b3f2f2de96ce42c
Dmytro Pahuba <dpaguba@gmail.com>
2024-06-28 00:23:32 +0200

    feat: Add sign-in form to login page

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+// import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+  form: FormGroup = new FormGroup(
+    {
+      username: new FormControl(null),
+      password: new FormControl(null),
+    }
+  )
+
+  onSubmit(event: Event): void{
+    console.log(event);
+    
+  }
 
 }
