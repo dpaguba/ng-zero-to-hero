1bfb1db7d0e569fa257ae040731fa6b4d59f4aae
Dmytro Pahuba <dpaguba@gmail.com>
2024-06-27 19:38:26 +0200

    refactor: Update profile card component to use dynamic data and improve styling

--- src/app/common-ui/profile-card/profile-card.component.ts
+++ src/app/common-ui/profile-card/profile-card.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Profile } from '../../data/interfaces/profile.interface';
+import { ImgUrlPipe } from "../../helpers/pipes/img-url.pipe";
 
 @Component({
-  selector: 'app-profile-card',
-  standalone: true,
-  imports: [],
-  templateUrl: './profile-card.component.html',
-  styleUrl: './profile-card.component.scss'
+    selector: 'app-profile-card',
+    standalone: true,
+    templateUrl: './profile-card.component.html',
+    styleUrl: './profile-card.component.scss',
+    imports: [ImgUrlPipe]
 })
 export class ProfileCardComponent {
-
+  @Input() profile!: Profile;
 }
