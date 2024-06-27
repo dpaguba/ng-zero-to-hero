b9b5624ae7178c70e7baef64207274119dd4054d
Dmytro Pahuba <dpaguba@gmail.com>
2024-06-28 01:20:12 +0200

    chore: Add access guard for authentication on protected routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 import { LayoutComponent } from './common-ui/layout/layout.component';
+import { canActiveAuth } from './auth/access.guard';
 
 export const routes: Routes = [
     {
@@ -15,7 +16,8 @@ export const routes: Routes = [
                 path: 'profile',
                 component: ProfilePageComponent
             }
-        ]
+        ],
+        canActivate: [canActiveAuth]
     },
     {
         path: 'login',
