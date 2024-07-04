bdb83647928af01f43d10384861795e64606172f
Dmytro Pahuba <dpaguba@gmail.com>
2024-07-04 23:53:51 +0200

    chore: Add subscriber card component and styles

---
+++ src/app/common-ui/subscriber-card/subscriber-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { Profile } from '../../data/interfaces/profile.interface';
+import { ImgUrlPipe } from "../../helpers/pipes/img-url.pipe";
+
+@Component({
+    selector: 'app-subscriber-card',
+    standalone: true,
+    templateUrl: './subscriber-card.component.html',
+    styleUrl: './subscriber-card.component.scss',
+    imports: [ImgUrlPipe]
+})
+export class SubscriberCardComponent {
+
+  @Input() profile!: Profile;
+}
